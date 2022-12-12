14e92e5edcf7ddd9a04e8c32567fd799fd0e7e69
bumman <anhbumbk@gmail.com>
2022-12-13 00:24:29 +0700

    login

---
+++ src/router.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import Router from 'vue-router';
+import Home from '@/components/Home.vue';
+import Login from '@/components/auth/Login.vue';
+import Profile from '@/components/auth/Profile.vue';
+
+Vue.use(Router);
+
+export const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      comments: Home
+    },
+    {
+      path: '/login',
+      comments: Login
+    },
+    {
+      path: '/profile',
+      comments: Profile
+    }
+  ]
+})
+
+// router.beforeEach((to, from, next) => {
+//   const publicPages = ['/login', '/register', '/home'];
+//   const authRequired = !publicPages.includes(to.path);
+//   const loggedIn = localStorage.getItem('user');
+
+//   // trying to access a restricted page + not logged in
+//   // redirect to login page
+//   if (authRequired && !loggedIn) {
+//     next('/login');
+//   } else {
+//     next();
+//   }
+// });
