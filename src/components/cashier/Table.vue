d7079a65f3a173fea559ca2245241bd059617294
hoanganh.pham <anh.phamhoang@vmg.vn>
2023-01-18 15:13:23 +0700

    select table

--- src/components/cashier/Table.vue
+++ src/components/cashier/Table.vue
@@ -4,7 +4,7 @@
 			<el-col v-for="item in tables" :key="item.id" :span="4" >
 				<!-- <font-awesome-icon icon="fa-solid fa-table-cells-large"/> -->
 				<!-- <svg xmlns="http://www.w3.org/2000/svg"> -->
-					<div @click="clickSelectTable(item.id)" class="table-item">
+					<div @click="clickSelectTable(item.id)" class="table-item" :class="{ table_actived: item.active }">
 						<img src="../../../public/static/icon/table.png"  />
 						{{ item.name }}
 					</div>
@@ -48,14 +48,19 @@ export default {
 	display: grid;
 	justify-items: center;
 	align-content: center;
+	padding: 10px;
+  border-radius: 10px;
+	margin: 10px;
+	/* cursor: -webkit-grab; cursor: grab; */
 }
 
-.table-selected {
-	background: #c2e0ff;
-}
-
-.table-actived {
+.table_selected {
 	background: #0066cc;
+}
+
+.table_actived {
+	
+	background: #c2e0ff;
 }
 
 </style>
